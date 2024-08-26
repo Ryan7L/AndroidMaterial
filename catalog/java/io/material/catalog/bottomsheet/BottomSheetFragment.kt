af1feaae949b6c4fe4cf08fe8277dcf7b9f39591
RyanLDL <2931378099@qq.com>
2024-08-26 11:44:31 +0800

    rename bottomsheet

--- catalog/java/io/material/catalog/bottomsheet/BottomSheetFragment.kt
+++ catalog/java/io/material/catalog/bottomsheet/BottomSheetFragment.kt
@@ -1,90 +1,63 @@
+package io.material.catalog.bottomsheet
 
-/*
- * Copyright 2018 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import androidx.fragment.app.Fragment
+import dagger.Provides
+import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoSet
+import io.material.catalog.R
+import io.material.catalog.application.scope.ActivityScope
+import io.material.catalog.application.scope.FragmentScope
+import io.material.catalog.feature.Demo
+import io.material.catalog.feature.DemoLandingFragment
+import io.material.catalog.feature.FeatureDemo
 
-package io.material.catalog.bottomsheet;
+class BottomSheetFragment : DemoLandingFragment() {
+  /**
+   * ActionBar 或 ToolBar 的标题的资源ID
+   */
+  override val titleResId: Int
+    get() = R.string.cat_bottomsheet_title
 
-import io.material.catalog.R;
+  /**
+   * 演示功能的描述的资源ID
+   */
+  override val descriptionResId: Int
+    get() = R.string.cat_bottomsheet_description
 
-import androidx.fragment.app.Fragment;
-import dagger.Provides;
-import dagger.android.ContributesAndroidInjector;
-import dagger.multibindings.IntoSet;
-import io.material.catalog.application.scope.ActivityScope;
-import io.material.catalog.application.scope.FragmentScope;
-import io.material.catalog.feature.Demo;
-import io.material.catalog.feature.DemoLandingFragment;
-import io.material.catalog.feature.FeatureDemo;
-import java.util.ArrayList;
-import java.util.List;
-
-/** A landing fragment that links to BottomSheet demos for the Catalog app. */
-public class BottomSheetFragment extends DemoLandingFragment {
-
-  @Override
-  public int getTitleResId() {
-    return R.string.cat_bottomsheet_title;
-  }
-
-  @Override
-  public int getDescriptionResId() {
-    return R.string.cat_bottomsheet_description;
-  }
-
-  @Override
-  public Demo getMainDemo() {
-    return new Demo() {
-      @Override
-      public Fragment getFragment() {
-        return new BottomSheetMainDemoFragment();
+  /**
+   * 主要的Demo
+   */
+  override val mainDemo: Demo
+    get() = object : Demo() {
+      override val fragment: Fragment
+        get() = BottomSheetMainDemoFragment()
+    }
+  override val additionalDemos: List<Demo>
+    get() = listOf(
+      object : Demo(R.string.cat_bottomsheet_scrollable_content_demo_title) {
+        override val fragment: Fragment
+          get() = BottomSheetScrollableContentDemoFragment()
       }
-    };
-  }
+    )
+}
 
-  @Override
-  public List<Demo> getAdditionalDemos() {
-    List<Demo> additionalDemos = new ArrayList<>();
-    additionalDemos.add(
-        new Demo(R.string.cat_bottomsheet_scrollable_content_demo_title) {
-          @Override
-          public Fragment getFragment() {
-            return new BottomSheetScrollableContentDemoFragment();
-          }
-        });
-    return additionalDemos;
-  }
+@dagger.Module
+abstract class BottomSheetModule {
 
-  /** The Dagger module for {@link BottomSheetMainDemoFragment} dependencies. */
-  @dagger.Module
-  public abstract static class Module {
+  @FragmentScope
+  @ContributesAndroidInjector
+  abstract fun contributeInjector(): BottomSheetFragment
 
-    @FragmentScope
-    @ContributesAndroidInjector
-    abstract BottomSheetFragment contributeInjector();
-
+  companion object {
+    @JvmStatic
     @IntoSet
     @Provides
     @ActivityScope
-    static FeatureDemo provideFeatureDemo() {
-      return new FeatureDemo(R.string.cat_bottomsheet_title, R.drawable.ic_bottomsheet) {
-        @Override
-        public Fragment getLandingFragment() {
-          return new BottomSheetFragment();
-        }
-      };
+    fun provideFeatureDemo(): FeatureDemo {
+      return object : FeatureDemo(R.string.cat_bottomsheet_title, R.drawable.ic_bottomsheet) {
+        override val landingFragment: Fragment
+          get() = BottomSheetFragment()
+      }
     }
   }
 }
--- catalog/java/io/material/catalog/bottomsheet/BottomSheetScrollableContentDemoFragment.kt
+++ catalog/java/io/material/catalog/bottomsheet/BottomSheetScrollableContentDemoFragment.kt
@@ -1,86 +1,64 @@
-/*
- * Copyright 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package io.material.catalog.bottomsheet
 
-package io.material.catalog.bottomsheet;
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.internal.ViewUtils
+import io.material.catalog.R
+import io.material.catalog.feature.DemoFragment
+import io.material.catalog.windowpreferences.WindowPreferencesManager
 
-import io.material.catalog.R;
-
-import android.app.Dialog;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.annotation.LayoutRes;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.view.WindowInsetsCompat;
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
-import com.google.android.material.internal.ViewUtils;
-import io.material.catalog.feature.DemoFragment;
-import io.material.catalog.windowpreferences.WindowPreferencesManager;
-
-/**
- * A fragment that displays the a BottomSheet demo with vertical scrollable content for the Catalog
- * app.
- */
-public class BottomSheetScrollableContentDemoFragment extends DemoFragment {
-  @Override
-  public View onCreateDemoView(
-      LayoutInflater layoutInflater, @Nullable ViewGroup viewGroup, @Nullable Bundle bundle) {
-    View view = layoutInflater.inflate(getDemoContent(), viewGroup, false /* attachToRoot */);
-    View button = view.findViewById(R.id.bottomsheet_button);
-    button.setOnClickListener(v -> new BottomSheet().show(getParentFragmentManager(), ""));
-    return view;
+class BottomSheetScrollableContentDemoFragment : DemoFragment() {
+  override fun onCreateDemoView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    val view =
+      inflater.inflate(R.layout.cat_bottomsheet_scrollable_content_fragment, container, false)
+    val button = view.findViewById<View>(R.id.bottomsheet_button)
+    button.setOnClickListener {
+      BottomSheet().show(parentFragmentManager, "")
+    }
+    return view
   }
 
-  @LayoutRes
-  protected int getDemoContent() {
-    return R.layout.cat_bottomsheet_scrollable_content_fragment;
-  }
-
-  /** A custom bottom sheet dialog fragment. */
-  @SuppressWarnings("RestrictTo")
-  public static class BottomSheet extends BottomSheetDialogFragment {
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-      // Set up BottomSheetDialog
-      BottomSheetDialog bottomSheetDialog =
-          new BottomSheetDialog(
-              getContext(), R.style.ThemeOverlay_Catalog_BottomSheetDialog_Scrollable);
-      new WindowPreferencesManager(requireContext()).applyEdgeToEdgePreference(bottomSheetDialog.getWindow());
-      bottomSheetDialog.setContentView(R.layout.cat_bottomsheet_scrollable_content);
-      View bottomSheetInternal = bottomSheetDialog.findViewById(R.id.design_bottom_sheet);
-      BottomSheetBehavior.from(bottomSheetInternal).setPeekHeight(400);
-
-      View bottomSheetContent = bottomSheetInternal.findViewById(R.id.bottom_drawer_2);
-      ViewUtils.doOnApplyWindowInsets(bottomSheetContent, (v, insets, initialPadding) -> {
-        // Add the inset in the inner NestedScrollView instead to make the edge-to-edge behavior
-        // consistent - i.e., the extra padding will only show at the bottom of all content, i.e.,
-        // only when you can no longer scroll down to show more content.
+  class BottomSheet : BottomSheetDialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+      val bottomSheetDialog = BottomSheetDialog(
+        requireContext(),
+        R.style.ThemeOverlay_Catalog_BottomSheetDialog_Scrollable
+      )
+      WindowPreferencesManager(requireContext()).applyEdgeToEdgePreference(bottomSheetDialog.window)
+      bottomSheetDialog.setContentView(R.layout.cat_bottomsheet_scrollable_content)
+      val bottomSheetInternal = bottomSheetDialog.findViewById<View>(R.id.design_bottom_sheet)
+      BottomSheetBehavior.from(bottomSheetInternal!!).peekHeight = 400
+      val bottomSheetContent = bottomSheetInternal.findViewById<View>(R.id.bottom_drawer_2)
+      ViewUtils.doOnApplyWindowInsets(
+        bottomSheetContent
+      ) { view, insets, initialPadding ->
+        /**
+         * When [set][View.setOnApplyWindowInsetsListener] on a
+         * View, this listener method will be called instead of the view's own [ ][View.onApplyWindowInsets] method. The `initialPadding` is the view's
+         * original padding which can be updated and will be applied to the view automatically. This
+         * method should return a new [WindowInsetsCompat] with any insets consumed.
+         */
+        //在内部 NestedScrollView 中添加插图，以使边缘到边缘的行为一致 - 即，额外的填充将仅显示在所有内容的底部，即仅当您无法再向下滚动以显示更多内容时。
         bottomSheetContent.setPaddingRelative(
-            initialPadding.start,
-            initialPadding.top,
-            initialPadding.end,
-            initialPadding.bottom + insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom);
-        return insets;
-      });
-      return bottomSheetDialog;
+          initialPadding.start,
+          initialPadding.top,
+          initialPadding.end,
+          initialPadding.bottom + insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom
+        )
+        return@doOnApplyWindowInsets insets
+      }
+      return bottomSheetDialog
     }
   }
 }
