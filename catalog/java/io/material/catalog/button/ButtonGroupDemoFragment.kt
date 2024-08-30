51ef460d5268d8625d5303dca1e88c12cb3182a1
RyanLDL <2931378099@qq.com>
2024-08-30 11:47:55 +0800

    upgrade lib、button、assets、docs、README

--- catalog/java/io/material/catalog/button/ButtonGroupDemoFragment.kt
+++ catalog/java/io/material/catalog/button/ButtonGroupDemoFragment.kt
@@ -12,15 +12,20 @@ import io.material.catalog.feature.DemoFragment
 import io.material.catalog.feature.DemoUtils
 
 class ButtonGroupDemoFragment : DemoFragment() {
-  private var defaultInset: Int = 0
   override fun onCreateDemoView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
     val view = inflater.inflate(R.layout.cat_buttons_group_fragment, container, false)
+
+    val content = view.findViewById<ViewGroup>(R.id.button_group_content)
+    inflater.inflate(iconOnlyButtonGroupContent, content, true)
+    inflater.inflate(labelOnlyButtonGroupContent, content, true)
+    inflater.inflate(mixedButtonGroupContent, content, true)
+
     val buttonGroups = DemoUtils.findViewsWithType(view, MaterialButtonGroup::class.java)
-    defaultInset = resources.getDimensionPixelSize(R.dimen.mtrl_btn_inset)
+
     val verticalOrientationToggle =
       view.findViewById<MaterialSwitch>(R.id.orientation_switch_toggle)
     verticalOrientationToggle.setOnCheckedChangeListener { buttonView, isChecked ->
@@ -28,13 +33,6 @@ class ButtonGroupDemoFragment : DemoFragment() {
         val orientation =
           if (isChecked) MaterialButtonGroup.VERTICAL else MaterialButtonGroup.HORIZONTAL
         it.orientation = orientation
-        for (i in 0 until it.childCount) {
-          val inset = getInsetForOrientation(orientation)
-          val button = it.getChildAt(i) as MaterialButton
-          button.insetBottom = inset
-          button.insetTop = inset
-          adjustParams(button.layoutParams, orientation)
-        }
         it.requestLayout()
       }
     }
@@ -44,13 +42,24 @@ class ButtonGroupDemoFragment : DemoFragment() {
     }
     return view
   }
+//  for (i in 0 until it.childCount) {
+//    val inset = getInsetForOrientation(orientation)
+//    val button = it.getChildAt(i) as MaterialButton
+//    button.insetBottom = inset
+//    button.insetTop = inset
+//    adjustParams(button.layoutParams, orientation)
+//  }
+//  private fun getInsetForOrientation(orientation: Int): Int {
+//    return if (orientation == MaterialButtonGroup.VERTICAL) 0 else defaultInset
+//  }
 
-  private fun getInsetForOrientation(orientation: Int): Int {
-    return if (orientation == MaterialButtonGroup.VERTICAL) 0 else defaultInset
-  }
+//  private fun adjustParams(layoutParams: ViewGroup.LayoutParams, orientation: Int) {
+//    layoutParams.width =
+//      if (orientation == MaterialButtonGroup.VERTICAL) ViewGroup.LayoutParams.MATCH_PARENT else ViewGroup.LayoutParams.WRAP_CONTENT
+//  }
 
-  private fun adjustParams(layoutParams: ViewGroup.LayoutParams, orientation: Int) {
-    layoutParams.width =
-      if (orientation == MaterialButtonGroup.VERTICAL) ViewGroup.LayoutParams.MATCH_PARENT else ViewGroup.LayoutParams.WRAP_CONTENT
-  }
+  private val iconOnlyButtonGroupContent = R.layout.cat_button_group_content_icon_only
+
+  private val labelOnlyButtonGroupContent = R.layout.cat_button_group_content_label_only
+  private val mixedButtonGroupContent = R.layout.cat_button_group_content_mixed
 }
