a0623193cbe20fbc2889a1de43c8ec9c848412ce
RyanLDL <2931378099@qq.com>
2024-08-30 11:28:52 +0800

    rename imageview

--- catalog/java/io/material/catalog/tableofcontents/TocModule.kt
+++ catalog/java/io/material/catalog/tableofcontents/TocModule.kt
@@ -22,6 +22,7 @@ import io.material.catalog.elevation.ElevationModule
 import io.material.catalog.fab.FabModule
 import io.material.catalog.font.FontFragment
 import io.material.catalog.font.FontModule
+import io.material.catalog.imageview.ImageViewModule
 import io.material.catalog.imageview.ShapeableImageViewFragment
 import io.material.catalog.loadingindicator.LoadingIndicatorFragment
 import io.material.catalog.materialswitch.SwitchFragment
@@ -43,7 +44,7 @@ import io.material.catalog.transition.TransitionModule
 
 @dagger.Module(
   includes = [AdaptiveFragment.Module::class, BottomAppBarModule::class, ButtonModule::class, BottomNavigationModule::class, BottomSheetModule::class, CardModule::class, CarouselModule::class, CheckBoxModule::class, ChipModule::class, ColorsFragment.Module::class, DatePickerModule::class, DialogModule::class, DividerModule::class, ElevationModule::class, FabModule::class, FontModule::class, MenuFragment.Module::class, NavigationDrawerFragment.Module::class, NavigationRailFragment.Module::class, ProgressIndicatorFragment.Module::class, RadioButtonFragment.Module::class,
-    SearchFragmentModule::class, ShapeableImageViewFragment.Module::class, BaseModule::class, LoadingIndicatorFragment.Module::class, ShapeThemingFragment.Module::class, SideSheetFragment.Module::class, SliderFragment.Module::class, SwitchFragment.Module::class, TabsFragment.Module::class, TextFieldFragment.Module::class, TimePickerDemoLandingFragment.Module::class, TopAppBarModule::class, TransitionModule::class, AnimationFragmentModule::class]
+    SearchFragmentModule::class, ImageViewModule::class, BaseModule::class, LoadingIndicatorFragment.Module::class, ShapeThemingFragment.Module::class, SideSheetFragment.Module::class, SliderFragment.Module::class, SwitchFragment.Module::class, TabsFragment.Module::class, TextFieldFragment.Module::class, TimePickerDemoLandingFragment.Module::class, TopAppBarModule::class, TransitionModule::class, AnimationFragmentModule::class]
 )
 abstract class TocModule {
   @FragmentScope
